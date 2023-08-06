2228c37b7fcd1df240040bc5c320ef6e3718a86e
baovit72 <17520267@gm.uit.edu.vn>
2023-08-06 10:26:58 +0700

    feat: add theme configs

---
+++ src/theme.js
@@ -0,0 +1,22 @@
+const theme = {
+  spacing: ['0', '0.25rem', '0.5rem', '1rem', '1.5rem', '2rem'],
+  fontSize: ['0.75rem', '0.875rem', '1rem', '1.25rem', '1.5rem', '2rem'],
+  fontFamily: {
+    number: 'Montserrat Alternates, sans-serif',
+    text: 'Readiness, sans-serif',
+  },
+  fontWeight: {
+    light: 300,
+    regular: 400,
+    bold: 700,
+  },
+  breakpoints:{
+    xs:"576px",
+    sm:"768px",
+    md:"992px",
+    lg:"1200px",
+    xl:"1400px"
+  } 
+};
+
+export default theme;
